614eaab2f0da8137aed46b30da0011cb1d45e6b3
Vijay Kumar <saroyav3@gmail.com>
2024-02-09 22:14:23 +1300

    first commit

---
+++ python_app.py
@@ -0,0 +1,13 @@
+from email.message import EmailMessage
+from app2 import password
+
+email_sender = 'saroyav3@gmail.com'
+email_password = password
+
+email_receiver = ''
+
+subject = "email from vijay"
+
+body = """
+hi you received this email through python code
+"""
