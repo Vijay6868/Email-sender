291a28d5435e2eedf94cf0d54c28cbda57770bfc
Vijay Kumar <saroyav3@gmail.com>
2024-02-21 21:01:52 +1300

    made added context to smtp

--- python_app.py
+++ python_app.py
@@ -1,5 +1,8 @@
 from email.message import EmailMessage
 from app2 import password
+import ssl
+import smtplib
+
 
 email_sender = 'saroyav3@gmail.com'
 email_password = password
@@ -11,3 +14,14 @@ subject = "email from vijay"
 body = """
 hi you received this email through python code
 """
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['sub'] = subject
+
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465, context=context) as smtp:
+    
